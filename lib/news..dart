86e66732ce05673137195d145a6b4fa5ea2b365d
burak <dev.burakuzun@gmail.com>
2025-01-18 19:04:28 +0300

    s

--- lib/news..dart
+++ lib/news..dart
@@ -80,4 +80,132 @@ const List<Map<String, dynamic>> newsData = [
     "index": 8,
     "category": 3 // Hobby
   },
+  {
+    "baslik":
+        "President of the Turkish Ophthalmology Association, Prof. Dr. Huban Atilla, stated that complaints about dry eyes in young children are increasing, emphasizing that prolonged screen time increases the risks of dry eyes and strabismus.",
+    "aciklama":
+        "The prevalence of dry eyes in the community is increasing. Atilla noted that they categorize the prevalence of diseases into childhood and adulthood and made the following remarks: In childhood, looking closely at screens, especially watching tablets and phones at night with lights off, increases myopia. Working closely with screens disrupts clear vision in the eyes, leading to a rise in myopia cases. Prolonged close-up work in children, particularly if there is an uncorrected need for glasses, can trigger eye misalignment, or strabismus. During the COVID-19 period, the increased use of tablets and phones for spending time and online lessons caused a rise in inward eye misalignment. We have started seeing early-onset dry eye disease more frequently in children. The prevalence of dry eye disease is increasing across all age groups. The most significant reason for this is improper use of light-emitting devices such as tablets, phones, and computers.",
+    "imageAsset": "assets/image1.jpeg",
+    "groupList": [0],
+    "index": 9,
+    "category": 3 // Hobby
+  },
+  {
+    "baslik": "Beware of 'dry eyes' risk in winter!",
+    "aciklama":
+        "Assoc. Prof. Dr. Hasan Kiziltoprak, Head of the Ophthalmology Department at Atlas University Faculty of Medicine, stated that dry and cold weather in winter, along with heating systems used in enclosed spaces, lowers humidity in the air, leading to dry eyes. Can be triggered during menopause Dry eyes are primarily caused by prolonged computer or phone use, exposure to windy or dry environments, and decreased tear production due to aging. Kiziltoprak warned, 'Additionally, hormonal changes, particularly during menopause, frequently trigger dry eyes. Furthermore, the use of antihistamines, antidepressants, or beta blockers can negatively affect tear production.'",
+    "imageAsset": "assets/image2.png",
+    "groupList": [0],
+    "index": 10,
+    "category": 3 // Hobby
+  },
+  {
+    "baslik":
+        "In Sanliurfa, Prof. Dr. Ali Simsek, Head of the Ophthalmology Department at Harran University Faculty of Medicine, stated that seasonal transitions and prolonged screen time are among the most significant causes of tear deficiency, known as dry eye disease.",
+    "aciklama":
+        "Prof. Dr. Ali Simsek provided important insights into dry eye disease. He stated, 'Dry eyes are often misunderstood. It is a symptom complex ranging from insufficient tear quantity to deficiencies in its composition and quality. Modern lifestyle factors, such as excessive screen time, significantly contribute to the prevalence of dry eye disease. Children spending hours on phones is a notable reason for dry eyes. Environmental factors like seasonal changes, high temperatures, dusty environments, or prolonged exposure to air conditioning also exacerbate the condition. Contact lens misuse, poor hygiene, and inadequate choices further worsen dry eyes. Symptoms include a sensation of dryness, irritation, burning, and even blurred vision. Patients are advised to limit screen time, maintain proper environmental conditions, and seek medical treatments such as artificial tears or medications for inflammation if needed.'",
+    "imageAsset": "assets/image3.png",
+    "groupList": [0],
+    "index": 11,
+    "category": 3 // Hobby
+  },
+  {
+    "baslik": "Not every kidney tumor is cancer!",
+    "aciklama":
+        "Although kidney tumors are more common in men, occurring in one out of every 50, they rank among the top 10 cancers diagnosed globally. In the U.S., approximately 65,000 new cases of kidney cancer are reported annually. Symptoms such as blood in the urine, weight loss, and appetite loss can signal kidney tumors. However, experts stress that not every kidney tumor is cancerous and emphasize the importance of accurate treatment approaches. Prof. Dr. Serdar Aykan from Memorial Bahcelievler Hospital stated that obesity, smoking, occupational chemical exposure, long-term kidney stones, and genetic factors increase kidney tumor risks. Tumors are usually slow-growing, with annual growth rates of up to 0.7 millimeters. The risk of metastasis depends on the tumor's size, being as low as 3-5% for tumors smaller than 4 cm.",
+    "imageAsset": "assets/image5.jpeg",
+    "groupList": [1],
+    "index": 12,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Kidney cancer cases have been increasing in recent years!",
+    "aciklama":
+        "Assoc. Prof. Dr. Hasan Turgut stated that kidney tumors, accounting for about 4% of all cancers, have seen a rise in recent years. Early diagnosis and surgical treatment yield successful outcomes. Smoking and obesity are known to increase the risk of kidney cancer, he added.",
+    "imageAsset": "assets/image6.png",
+    "groupList": [1],
+    "index": 13,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Dry eye can lead to serious long-term problems, warns ophthalmologist Dr. Ceyhun Ozkok.",
+    "aciklama":
+        "Ophthalmologist Dr. Ceyhun Ozkok emphasized that tear deficiency can cause significant long-term issues and, in rare cases, even blindness. Dry eye symptoms arise when there is insufficient tear production or poor tear quality. The tear film consists of three layers: mucous, aqueous, and lipid. A deficiency or dysfunction in any of these layers can lead to irritation and reduced vision. Factors like hormonal changes, aging, and menopause further increase the risk. Dr. Ozkok advised seeking medical attention for symptoms and stressed the importance of preventive measures such as limiting screen time and maintaining proper hydration.",
+    "imageAsset": "assets/image9.jpeg",
+    "groupList": [2],
+    "index": 14,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "TREATMENT FOR DRY EYE FOCUSES ON INCREASING TEARS",
+    "aciklama":
+        "Dr. Ozkok stated that increasing tear production is key in treating dry eyes. Artificial tears and gels are commonly recommended. In severe cases, temporary tear duct plugs or permanent closure using laser may be applied to reduce tear loss. Tips to prevent dry eyes include consciously blinking at intervals, avoiding direct air from devices like air conditioners and hairdryers, and wearing protective eyewear in windy conditions. Rubbing eyes should also be avoided.",
+    "imageAsset": "assets/image9.jpeg",
+    "groupList": [2],
+    "index": 15,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Smoking increases the risk of eye diseases and vision loss.",
+    "aciklama":
+        "Smoking negatively impacts eye health, increasing the risk of conditions like macular degeneration, cataracts, glaucoma, dry eye disease, and uveitis. The toxic chemicals in cigarette smoke can irritate the eye's surface, promote blood clotting, and damage blood vessels, leading to serious vision problems. Passive smoking also doubles the risk of developing dry eye syndrome, exacerbating symptoms like burning, irritation, and blurred vision. Protective measures and smoking cessation are crucial for preserving eye health.",
+    "imageAsset": "assets/image10.jpeg",
+    "groupList": [2],
+    "index": 16,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Excessive vitamin D can be harmful.",
+    "aciklama":
+        "Vitamin D is essential for a healthy life but must be taken under medical supervision to avoid adverse effects. While the sun is a primary source, improper supplementation can lead to complications, especially in at-risk groups. Sufficient sunlight exposure during specific hours is key to adequate vitamin D synthesis. Prof. Dr. Mazhar Muslim Tuna highlighted the importance of proper timing and supplementation for effective absorption and utilization of vitamin D.",
+    "imageAsset": "assets/image13.jpeg",
+    "groupList": [3],
+    "index": 17,
+    "category": 1 // Hobby
+  },
+  {
+    "baslik": "Does Vitamin D Deficiency Cause Hypertension?",
+    "aciklama":
+        "Internal Medicine Specialist Dr. Sekip Altunkan stated that hypertension is a multifactorial global disease with rising prevalence. While studies in animals suggest a positive impact of vitamin D on hypertension and heart diseases, human studies remain inconclusive. Dr. Altunkan cautioned against overuse of vitamin D supplements, emphasizing the need for balance as excessive doses can have harmful effects.",
+    "imageAsset": "assets/image14.jpeg",
+    "groupList": [3],
+    "index": 18,
+    "category": 1 // Hobby
+  },
+  {
+    "baslik": "Vitamin D deficiency increases dementia risk.",
+    "aciklama":
+        "Research indicates that elderly individuals with vitamin D deficiency face a higher risk of developing dementia. The study, published in Neurology, examined 1,650 people aged 65 and above. While earlier studies also linked vitamin D deficiency with dementia, researchers claim this is the most comprehensive report to date.",
+    "imageAsset": "assets/image15.png",
+    "groupList": [3],
+    "index": 19,
+    "category": 1 // Hobby
+  },
+  {
+    "baslik": "Prof. Dr. Akar Provided Information on Rheumatic Diseases in Giresun",
+    "aciklama":
+        "Prof. Dr. Servet Akar from Izmir Katip Celebi University's Rheumatology Department visited Giresun Education and Research Hospital, delivering a presentation on Imaging Methods in Rheumatic Diseases. The event included a plaque of appreciation presented by the hospital's Chief Physician and attracted significant interest.",
+    "imageAsset": "assets/image16.png",
+    "groupList": [4],
+    "index": 20,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Musculoskeletal System Pains in Children",
+    "aciklama":
+        "Musculoskeletal pain is a common reason for concern among families, occurring in 10-30% of children. While some cases stem from underlying conditions, most are benign, such as growth pains. Growth pains typically affect both sides, often in the thighs and calves, and usually occur in the evening or night. These pains are not serious and tend to resolve over time.",
+    "imageAsset": "assets/image17.png",
+    "groupList": [4],
+    "index": 21,
+    "category": 2 // Hobby
+  },
+  {
+    "baslik": "Office Life Can Cause Various Musculoskeletal Problems!",
+    "aciklama":
+        "Office workers frequently experience musculoskeletal issues, including neck, back, shoulder, and wrist pain. Poor posture and repetitive movements are major contributors. Prof. Dr. Nihal Ozaras emphasized the importance of ergonomic practices and physical activity to prevent these conditions, which negatively impact quality of life and productivity.",
+    "imageAsset": "assets/image18.pngo",
+    "groupList": [4],
+    "index": 22,
+    "category": 2 // Hobby
+  }
 ];
